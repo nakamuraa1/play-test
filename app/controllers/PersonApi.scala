7b040c1f5421f396528aa7d3934a2e7d489fe530
nakamuraa1 <akari.nakamura@opentone.co.jp>
2015-09-08 09:42:58 +0900

    項目追加、バリデーション追加

--- app/controllers/PersonApi.scala
+++ app/controllers/PersonApi.scala
@@ -3,19 +3,15 @@ package controllers
 import models.Person
 import play.api.libs.json.{JsError, Json}
 import play.api.mvc._
-/**
- * Created by a-nakamura on 2015/09/04.
- */
 class PersonApi extends Controller {
 
-    import formatters.Formatter.PersonJsonFormatter
+  import formatters.Formatter.PersonJsonFormatter
 
-  def personRegister = Action(parse.json){
-      _.body.validate[Person].map { p =>
-        Ok(Json.toJson(p))
-      }.recoverTotal { e =>
-        BadRequest(JsError.toJson(e))
-      }
+  def personRegister = Action(parse.json) {
+    _.body.validate[Person].map { p =>
+      Ok(Json.toJson(p))
+    }.recoverTotal { e =>
+      BadRequest(JsError.toJson(e))
     }
-
+  }
 }
--- test/personApiSpec.scala
+++ test/personApiSpec.scala
@@ -10,15 +10,15 @@ class PersonAPISpec extends Specification {
     "register person" in new WithApplication {
       val Some(result) = route(FakeRequest(POST, "/api/v1/person",
         FakeHeaders(Seq(CONTENT_TYPE -> "application/json")),
-        Json.parse( """{"age":24, "name":{"first":"FirstName", "last":"LastName"}, "bloodType":"AB", "favoriteNum":1}""")))
+        Json.parse( """{"age":24, "name":{"first":"FirstName", "last":"LastName"}, "bloodType":"AB", "favoriteNumbers":[1]}""")))
       status(result) mustEqual OK
-      contentAsString(result) mustEqual """{"age":24,"name":{"first":"FirstName","last":"LastName"}}"""
+      contentAsString(result) mustEqual """{"age":24,"name":{"first":"FirstName","last":"LastName"},"bloodType":"AB","favoriteNumbers":[1]}"""
     }
 
-    "display json parse error caused by PersonJsonFormatter" in new WithApplication {
+    "display json parse error caused by PersonJsonFormatter age" in new WithApplication {
       val Some(result) = route(FakeRequest(POST, "/api/v1/person",
         FakeHeaders(Seq(CONTENT_TYPE -> "application/json")),
-        Json.parse( """{"typo!!!":24, "name":{"first":"FirstName", "last":"LastName"}}""")))
+        Json.parse( """{"typo!!!":24, "name":{"first":"FirstName", "last":"LastName"}, "bloodType":"B", "favoriteNumbers":[1,2]}""")))
       status(result) mustEqual BAD_REQUEST
       contentAsString(result) mustEqual """{"obj.age":[{"msg":["error.path.missing"],"args":[]}]}"""
     }
@@ -26,10 +26,32 @@ class PersonAPISpec extends Specification {
     "display json parse error caused by NameJsonFormatter" in new WithApplication {
       val Some(result) = route(FakeRequest(POST, "/api/v1/person",
         FakeHeaders(Seq(CONTENT_TYPE -> "application/json")),
-        Json.parse( """{"age":24, "name":{"first":"FirstName", "typo!!!":"LastName"}}""")))
+        Json.parse( """{"age":24, "name":{"first":"FirstName", "typo!!!":"LastName"}, "bloodType":"B", "favoriteNumbers":[1,2]}""")))
       status(result) mustEqual BAD_REQUEST
-
       contentAsString(result) mustEqual """{"obj.name.last":[{"msg":["error.path.missing"],"args":[]}]}"""
+    }
+
+    "resisterPerson bloodType null" in new WithApplication {
+      val Some(result) = route(FakeRequest(POST, "/api/v1/person",
+        FakeHeaders(Seq(CONTENT_TYPE -> "application/json")),
+        Json.parse( """{"age":24, "name":{"first":"FirstName", "last":"LastName"}, "favoriteNumbers":[1,2]}""")))
+      status(result) mustEqual OK
+      contentAsString(result) mustEqual """{"age":24,"name":{"first":"FirstName","last":"LastName"},"favoriteNumbers":[1,2]}"""
+    }
+
+    "display json parse error caused by NameJsonFormatter favoriteNumbers" in new WithApplication {
+      val Some(result) = route(FakeRequest(POST, "/api/v1/person",
+        FakeHeaders(Seq(CONTENT_TYPE -> "application/json")),
+        Json.parse( """{"age":24, "name":{"first":"FirstName", "last":"LastName"}, "bloodType":"B", "favoriteNumbers":["a",2]}""")))
+      status(result) mustEqual BAD_REQUEST
+      contentAsString(result) mustEqual """{"obj.favoriteNumbers[0]":[{"msg":["error.expected.jsnumber"],"args":[]}]}"""
+    }
+    "display json parse error caused by age must be under 30" in new WithApplication {
+      val Some(result) = route(FakeRequest(POST, "/api/v1/person",
+        FakeHeaders(Seq(CONTENT_TYPE -> "application/json")),
+        Json.parse( """{"age":35, "name":{"first":"FirstName", "last":"LastName"}, "bloodType":"B", "favoriteNumbers":[0,2]}""")))
+      status(result) mustEqual BAD_REQUEST
+      contentAsString(result) mustEqual """{"obj.age":[{"msg":["age must be under 30"],"args":[]}]}"""
     }
   }
 }
